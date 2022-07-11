ff8f5bcef5da331f3a429da3e2106518907b25f2
cbel6500 <cbelser@confluent.io>
2022-07-11 12:55:37 -0400

    added some code

---
+++ something.py
@@ -0,0 +1,10 @@
+from random import randint
+
+def special_function(is_special):
+   if is_special:
+      return "this function is special"
+   else:
+      return "this function is not special"
+
+def guess_age()
+   return randint(0, 100)
